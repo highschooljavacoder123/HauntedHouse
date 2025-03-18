27c47ddf09082fb32e60712e2e015b5548dd4a4a
highschooljavacoder123 <as2233@pleasantonusd.net>
2025-03-18 21:03:25 +0000

    message

---
+++ HauntedHouse.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList; 
+
+public class HauntedHouse {
+    private Rooms[][] map = new Rooms[2][3];
+    public HauntedHouse(Rooms[][] createdmap) {
+        map = createdmap;
+    }
+}

dd059189c3fb36345b75b0e0f73f02fde4786524
abhayg0303 <3abhayg@gmail.com>
2025-03-26 21:30:52 +0000

    commit

---
+++ Room.java
@@ -0,0 +1,34 @@
+public class room{
+    private String name; 
+    private String description; 
+    private int darknessLevel; 
+    private Monster
+
+    public room(String x, String y, int z){
+        name = x; 
+        description = y; 
+        darknessLevel = z; 
+    }
+
+    public String getname(){
+        return name; 
+    }
+    public String getdescription(){
+        return description; 
+    }
+    public int getdarknesslevel(){
+        return darknessLevel; 
+    }
+    public void setName(String m){
+        name = atishay; 
+
+    }
+    public void setDescription(String jhxt){
+        description = abhay; 
+    }
+    public void setdarknesslevel(int p){
+        darknessLevel = p; 
+    }
+
+
+}
